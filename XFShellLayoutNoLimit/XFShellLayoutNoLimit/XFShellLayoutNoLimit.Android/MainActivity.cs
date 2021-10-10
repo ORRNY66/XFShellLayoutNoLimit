b112b85ccb4f63e483a2ba01e8c1443de6856ad9
ORRNY66 <ORRNY99@gmail.com>
2021-10-10 18:19:58 -0400

    Add files via upload

---
+++ XFShellLayoutNoLimit/XFShellLayoutNoLimit/XFShellLayoutNoLimit.Android/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Views;
+
+namespace XFShellLayoutNoLimit.Droid
+{
+    [Activity(Label = "XFShellLayoutNoLimit", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize |
+                               ConfigChanges.Orientation | 
+                               ConfigChanges.UiMode | 
+                               ConfigChanges.ScreenLayout |
+                               ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Window.AddFlags(WindowManagerFlags.LayoutNoLimits);
+            Window?.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+                uiOptions |= (int)WindowManagerFlags.LayoutNoLimits;
+
+                //uiOptions &= ~(int)WindowManagerFlags.TranslucentNavigation;
+                //uiOptions &= ~(int)WindowManagerFlags.TranslucentStatus;
+
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            }
+
+            Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            Window.ClearFlags(WindowManagerFlags.TranslucentNavigation);
+            Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            Window.SetNavigationBarColor(Android.Graphics.Color.Transparent);
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XFShellLayoutNoLimit/XFShellLayoutNoLimit/XFShellLayoutNoLimit/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XFShellLayoutNoLimit.Views;
+
+namespace XFShellLayoutNoLimit
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
